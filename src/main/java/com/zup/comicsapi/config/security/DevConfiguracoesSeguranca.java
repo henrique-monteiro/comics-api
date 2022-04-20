3caf9a3fedf2102ad5e737b65984802e61e72c69
Henrique De Moraes <henrique-monteiro@hotmail.com>
2022-04-20 12:38:33 -0300

    delete authentications for development environment

--- src/main/java/com/zup/comicsapi/config/security/DevConfiguracoesSeguranca.java
+++ src/main/java/com/zup/comicsapi/config/security/DevConfiguracoesSeguranca.java
@@ -1,59 +1,21 @@
 package com.zup.comicsapi.config.security;
 
-import com.zup.comicsapi.reposiroty.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @Profile("dev")
 public class DevConfiguracoesSeguranca extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private AutenticacaoService autenticacaoService;
-	
-	@Autowired
-	private TokenService tokenService;
-	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
-	@Override
-	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-	
-	@Override //configuracoes de autenticacao (controle de login)
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-	}
 	
 	@Override //configuracoes de autorizacao (URL's publicas e bloqueadas)
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").permitAll()
-								.and().csrf().disable()//desabilita a proteção contra o ataque hacker csrf (JSESSION)
-								;
+								.and().csrf().disable();//desabilita a proteção contra o ataque hacker csrf (JSESSION)
 	}
-	
-	@Override //configuracoes de recursos estaticos de frontend (js, css, imagens, etc..)
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
-	}
-	
-//	public static void main(String[] args) {
-//		System.out.println(new BCryptPasswordEncoder().encode("teste123"));
-//	}
 }
